5863187e7f818d719359f05692f5fe81251a0286
Danicho23 <henry.brito4040@gmail.com>
2020-07-25 08:50:22 -0500

    nuevos cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {AccordionModule} from 'primeng/accordion';  
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
