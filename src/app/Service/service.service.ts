f2d7af5404601876dd99af12308955465bff876e
Danicho23 <henry.brito4040@gmail.com>
2020-07-25 15:44:49 -0500

    tets

--- src/app/Service/service.service.ts
+++ src/app/Service/service.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { usuarios } from '../Modelo/usuarios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  constructor(private http : HttpClient ) { }
+ Url="aqyui po=nga su swager";
+
+ getUsers(){
+   return this.http.get<usuarios[]>(this.Url);
+ }
 }
