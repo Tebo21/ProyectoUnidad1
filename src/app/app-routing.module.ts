ee18e4f8d4e4f24bf2bc7e9cc2eef2e231c7f988
Tebo21 <franklinlucero62@gmail.com>
2020-07-25 10:13:50 -0500

    cambio formulario-ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  
+
+  {
+    path: 'registro',
+    component: FormularioUsuarioComponent
+  }
 ];
 
 @NgModule({
