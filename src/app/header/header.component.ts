eadf838878417c98abf27f6a8de8c0dd96c04ac2
Danicho23 <henry.brito4040@gmail.com>
2020-07-27 19:22:26 -0500

    menu_cambios

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,39 +2,48 @@ import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
- 
-  items: MenuItem[];
 
-  ngOnInit() {
-      this.items = [
-          {
-              label: 'File',
-              items: [{
-                      label: 'New', 
-                      icon: 'pi pi-fw pi-plus',
-                      items: [
-                          {label: 'Project'},
-                          {label: 'Other'},
-                      ]
-                  },
-                  {label: 'Open'},
-                  {label: 'Quit'}
-              ]
-          },
-          {
-              label: 'Edit',
-              icon: 'pi pi-fw pi-pencil',
-              items: [
-                  {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-                  {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-              ]
-          }
-      ];
-  }
+    items: MenuItem[];
+
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Video', icon: 'pi pi-fw pi-video',
+                items: [
+                    { label: 'Ver videos', icon: 'pi pi-fw pi-play' },
+                    { label: 'Subir video', icon: 'pi pi-fw pi-cloud-upload' }
+                ]
+            },
+            {
+                label: 'Fotos',
+                icon: 'pi pi-fw pi-camera',
+                items: [
+                    { label: 'Ver fotos', icon: 'pi pi-fw pi-images' },
+                    { label: 'Subir foto', icon: 'pi pi-fw pi-cloud-upload' }
+                ]
+            },
+            {
+                label: 'Documentos',
+                icon: 'pi pi-fw pi-file-o',
+                items: [
+                    { label: 'Ver documentos', icon: 'pi pi-fw pi-clone' },
+                    { label: 'Subir documentos', icon: 'pi pi-fw pi-cloud-upload' }
+                ]
+            },
+            {
+                label: 'Audio',
+                icon: 'pi pi-fw pi-file-o',
+                items: [
+                    { label: 'Audios', icon: 'pi pi-fw pi-volume-up' },
+                    { label: 'Subir documentos', icon: 'pi pi-fw pi-cloud-upload' }
+                ]
+            }
+        ];
+    }
 }
